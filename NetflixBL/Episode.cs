efbfed837b1de58eddb0d8537b36a788c30f24c1
רז משאט <razmashat@gmail.com>
2019-11-16 14:44:35 +0000

    episode and playlist builder

--- NetflixBL/Episode.cs
+++ NetflixBL/Episode.cs
@@ -1,12 +1,34 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NetflixDAL;
+using System.Data;
 
 namespace NetflixBL
 {
-    class Episode
+   public class Episode
     {
+        private int EpisdeID;
+        private string EpisodeName;
+        private int EpisodeLength;
+        private int EpiSeries;
+        private int SeasonNum;
+        private int EpiNum;
+
+
+        public Episode (int ID) 
+        {
+            DataSet ds = episodeDAL.GetepiByID(ID);
+            EpisdeID = int.Parse(ds.Tables["epiByIdTbl"].Rows[0]["EpisdeID"].ToString());
+            EpisodeName = ds.Tables["epiByIdTbl"].Rows[0]["EpisodeName"].ToString();
+            EpisodeLength = int.Parse(ds.Tables["epiByIdTbl"].Rows[0]["EpisodeLength"].ToString());
+            EpiSeries = int.Parse(ds.Tables["epiByIdTbl"].Rows[0]["EpiSeries"].ToString());
+            SeasonNum = int.Parse(ds.Tables["epiByIdTbl"].Rows[0]["SeasonNum"].ToString());
+            EpiNum = int.Parse(ds.Tables["epiByIdTbl"].Rows[0]["EpiNum"].ToString());
+            
+            
+        }
     }
 }
--- NetflixBL/playlist.cs
+++ NetflixBL/playlist.cs
@@ -1,12 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NetflixDAL;
+using System.Data;
 
 namespace NetflixBL
 {
-    class playlist
+    public class playlist
     {
+        private int PlayListID;
+        private string MyUser;
+
+        public playlist(int ID)
+        {
+            DataSet ds = playlistDAL.GetPlayListByID(ID);
+            PlayListID = int.Parse(ds.Tables["playlistByIdTbl"].Rows[0]["PlayListID"].ToString());
+            MyUser = ds.Tables["playlistByIdTbl"].Rows[0]["MyUser"].ToString();
+        }
     }
 }

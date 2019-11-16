7a38952d758668a748eab2aaa9167327988e9c28
רז משאט <razmashat@gmail.com>
2019-11-16 13:56:12 +0000

    sub builder

--- NetflixBL/Subscription.cs
+++ NetflixBL/Subscription.cs
@@ -1,12 +1,36 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NetflixDAL;
+using System.Data;
 
 namespace NetflixBL
 {
-    class Subscription
+    public class Subscription
     {
+
+
+    private int SubscriptionID;
+    private string description;
+    private int amountpermonth;
+    private int SeriesAmount;
+
+
+    public Subscription(int ID) {
+        
+     
+      DataSet ds = SubScriptionTypeTBLDAL.GetSubByID(ID);
+      SubscriptionID = int.Parse(ds.Tables["subByIdTbl"].Rows[0]["SubscriptionID"].ToString());
+      description = ds.Tables["subByIdTbl"].Rows[0]["description"].ToString();
+      amountpermonth = int.Parse(ds.Tables["subByIdTbl"].Rows[0]["amountpermonth"].ToString());
+      SeriesAmount = int.Parse(ds.Tables["subByIdTbl"].Rows[0]["SeriesAmount"].ToString());
+      
+
+  
+
+
+
     }
 }

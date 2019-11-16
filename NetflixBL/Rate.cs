0be336930eacf8c427ba8b00b43eefaf5d1a214a
רז משאט <razmashat@gmail.com>
2019-11-16 10:33:13 +0000

    Rate bl

--- NetflixBL/Rate.cs
+++ NetflixBL/Rate.cs
@@ -1,15 +1,38 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NetflixDAL;
-
+using System.Data;
 namespace NetflixBL
 {
-    class Rate
+   public class Rate
     {
  
+
+     private int RateID;
+     private int Starts;
+     private string Author;
+     private int EpisodeRated;
+
+
+     public Rate (int ID)
+     {
+
+       
+       DataSet ds = rateDAL.GetRateByID(ID);
+       RateID = int.Parse(ds.Tabels.Rows[0]["RateID"].ToString());
+       Starts = int.Parse(ds.Tabels.Rows[0]["Stars"].ToString());
+       Author = ds.Tabels.Rows[0]["Author"].ToString();
+       EpisodeRated = int.Parse(ds.Tabels.Rows[0]["EpisodeRated"].ToString());
+
+
+
+     }
+
+      
+
         
 
     }

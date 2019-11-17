22a1c6f392ec18624880e0957b7e243cc6963521
Raz Mashat <razmashat@Razs-iMac.local>
2019-11-17 16:30:20 +0200

    admin builder

--- NetflixBL/Admin.cs
+++ NetflixBL/Admin.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NetflixDAL;
+using System.Data;
 
 namespace NetflixBL
 {
-    class Admin
+    public class Admin
     {
+        private int AdminID;
+        private string AdminName;
+        private string AdminPassword;
+
+        public Admin(int ID) {
+            DataSet ds = AdminDAL.GetAdminByID(ID);
+            AdminID = int.Parse(ds.Tables["adminByIdTbl"].Rows[0]["AdminID"].ToString());
+            AdminName = ds.Tables["adminByIdTbl"].Rows[0]["AdminName"].ToString();
+            AdminPassword = ds.Tables["adminByIdTbl"].Rows[0]["AdminPassword"].ToString();
+
+        }
     }
 }

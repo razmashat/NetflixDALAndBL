ea5ac241e7d23bc0ee552a5ff39aab4652d4980a
Raz Mashat <razmashat@Razs-iMac.local>
2019-11-17 21:54:44 +0200

    final builders

--- NetflixBL/User.cs
+++ NetflixBL/User.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NetflixDAL;
+using System.Data;
 
 namespace NetflixBL
 {
-    class User
+    public class User
     {
+        private string username;
+        private string pass;
+        private int MyAdmin;
+        private int subscriptiontype;
+        private string email;
+
+        public User (string ID){
+
+            DataSet ds = UserDAL.GetuserByID(ID);
+            username = ds.Tables["adminByIdTbl"].Rows[0]["username"].ToString();
+            pass = ds.Tables["adminByIdTbl"].Rows[0]["pass"].ToString();
+            subscriptiontype = int.Parse(ds.Tables["adminByIdTbl"].Rows[0]["subscriptiontype"].ToString());
+            MyAdmin = int.Parse(ds.Tables["adminByIdTbl"].Rows[0]["MyAdmin"].ToString());
+            email = ds.Tables["adminByIdTbl"].Rows[0]["email"].ToString();
+            
+
+        }
     }
 }
